6838f54b6b57e8218fd36e40c7af4e38ac1dd589
Mcontre11 <78456343+Mcontre11@users.noreply.github.com>
2021-10-23 18:53:37 -0400

    commit

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
-
-var db, collection;
-
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
-
-app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
-    });
-});
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -24,38 +8,27 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+  res.render('index.ejs')
   })
-})
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
+  app.get('/api',(req,res) => {
+    if (req.query.word) {
+      let string = req.query.word
+      console.log("got this word from the browser",string)
+      let reverseStr = string.split('').filter(el => el !== ' ').reverse().join('')
+      let newStr = string.split('').filter(el => el !== ' ').join('')
+      let palindrome = reverseStr === newStr
+     
+      const objToJson = {
+        palindrome: palindrome
+      }
+      res.send(objToJson);
+    } else {
+        const objToJson = {
+        error: "Word Required"
+      }
+      res.send(objToJson);
     }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
+  
   })
-})
-
-app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
-    if (err) return res.send(500, err)
-    res.send('Message deleted!')
-  })
-})
+  app.listen(8000, console.log('running'));
